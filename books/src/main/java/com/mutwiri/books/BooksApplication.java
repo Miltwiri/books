9d37b16f60dd2127c57f2cebaa224ca3a51349ad
milton <miltonelim64@gmail.com>
2023-11-14 12:52:27 +0300

    Java Rest

---
+++ books/src/main/java/com/mutwiri/books/BooksApplication.java
@@ -0,0 +1,13 @@
+package com.mutwiri.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
